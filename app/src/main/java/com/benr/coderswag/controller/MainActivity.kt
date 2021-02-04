b7c926c46dd1007e55ef0f6279e44a6a68c8d26f
Ben Ross <benjamin.ross@youview.com>
2021-02-04 15:43:42 +0000

    Remove comment

--- app/src/main/java/com/benr/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/benr/coderswag/controller/MainActivity.kt
@@ -10,12 +10,3 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
-
-/*
- * Categories - SHIRTS, HOODIES, HATS - create an immutable array of categories
- * Products - create an array, each containing 4 products from each category (4 hats, 4 hoodies, etc.)
- * The arrays are immutable
- * Create the arrays inside of a singleton object - not sure what to call the file - check
- * Create a package called 'Services' where our singletons will live
- * Create a file called DataService.kt for our singleton object
- */
