ffdf4f13bff306ece15ee0fa70402fb13739a6a2
Ben Ross <benjamin.ross@youview.com>
2021-02-18 12:11:15 +0000

    Lesson 60: Simple ListView
    - Display the list of categories as a Simple ListView
    - Use a built-in Adapter and built-in Layout to transform an array of categories into a basic looking UI list
    - Start creating a custom Layout (category_list_item.xml) to display categories as images

--- app/src/main/java/com/benr/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/benr/coderswag/controller/MainActivity.kt
@@ -2,11 +2,56 @@ package com.benr.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.benr.coderswag.R
+import com.benr.coderswag.model.Category
+import com.benr.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * An Adapter is a bridge between the model layer and the view layer.
+     *
+     * Our Array Adapter takes our data (the Singleton categories array),
+     * transforms each array item into a View object (something that can be shown in the UI, e.g. buttons, spinners, etc. are Views)
+     * and then inserts those Views into the ListView (which groups and lists multiple Views together
+     * in whatever way you want).
+     *
+     * Create a variable for the Adapter outside of the onCreate lifecycle method
+     * so that we can reference it in different lifecycle methods if we want/need to.
+     */
+    lateinit var adapter: ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the array of Category instances
+        val categories = DataService.categories
+
+        /**
+         * Create the Adapter, passing it the context (the MainActivity), a built-in Android layout called 'simple_list_item_1' which
+         * specifies that each array item will be a TextView (a layout is a UI component used to define the structure of the UI (e.g.
+         * we add constraints using a ConstraintLayout), and the data we want to display (the array of Category instances)/
+         */
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, categories)
+
+        /**
+         * Get the ListView we want to display our category titles in
+         *
+         * FIXME: Kotlin Android Extensions (kotlinx) should be auto-importing
+         * the ListView when I start typing 'categoryListView' on its own, but it doesn't.
+         * It should be resulting in:
+         * ```kt
+         * import kotlinx.android.synthetic.main.activity_main.*
+         * ```
+         * but it doesn't.
+         * So for now I'm getting the View the old-fashioned way.
+         */
+        val listView = findViewById<ListView>(R.id.categoryListView)
+
+        // Tell the ListView to use our Array Adapter
+        listView.adapter = adapter
     }
 }
--- app/src/main/java/com/benr/coderswag/model/Category.kt
+++ app/src/main/java/com/benr/coderswag/model/Category.kt
@@ -1,3 +1,15 @@
 package com.benr.coderswag.model
 
-class Category(val title: String, val image: String)
+class Category(private val title: String, val image: String) {
+
+    /**
+     * When the Array Adapter uses our array of Category instances,
+     * without this override, the Category instances (i.e. objects)
+     * would be printed instead of the title strings. i.e. Android
+     * would instead print the IDs of where the data is stored in
+     * memory, rather than the titles.
+     */
+    override fun toString(): String {
+        return title
+    }
+}
