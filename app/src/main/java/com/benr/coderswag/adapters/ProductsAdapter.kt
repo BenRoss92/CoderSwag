7ec5da3a77b0c67819a6802c0331a10b14568827
Ben Ross <benjamin.ross@youview.com>
2021-05-27 16:59:36 +0100

    Lesson 66: Product Recycle Adapter - Create a custom Products RecyclerView Adapter to display a list of products - Products page is a grid. The number of columns in the grid increases when orientation is landscape and when device is an extra large size - Hook up Products Activity, Products RecyclerView Adapter and the product layouts (individual product and list of products)

---
+++ app/src/main/java/com/benr/coderswag/adapters/ProductsAdapter.kt
@@ -0,0 +1,51 @@
+package com.benr.coderswag.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.benr.coderswag.R
+import com.benr.coderswag.model.Product
+
+class ProductsAdapter(private val context: Context, private val products: List<Product>): RecyclerView.Adapter<ProductsAdapter.ProductHolder>() {
+
+    inner class ProductHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        // Get the UI elements (Views) from the View object being passed into the ViewHolder (ProductHolder) - Product image, product name view, product price view
+        private val productImageView = itemView.findViewById<ImageView>(R.id.productimage)
+        private val productNameView: TextView = itemView.findViewById(R.id.productname)
+        private val productPriceView = itemView.findViewById<TextView>(R.id.productprice)
+
+        fun bind(product: Product) {
+            // Get the correct image from the resource folder based on the product's image name
+                // You can't use the R object because you don't know what the name of the file is - it dynamically changes
+                // You can't use findViewById because it's not a view - it's a drawable file.
+            val imageId = context.resources.getIdentifier(product.image, "drawable", context.packageName)
+
+            // set the product image view to be the product's image name
+            productImageView.setImageResource(imageId)
+
+            // set the product name view to be the product's name
+            productNameView.text = product.title
+            // set the product price view to be the product's price
+            productPriceView.text = product.price
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
+        // get the view by inflating it from the layout file
+        val productView = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false)
+        return ProductHolder(productView)
+    }
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        holder.bind(products[position])
+    }
+
+    override fun getItemCount(): Int {
+        return products.count()
+    }
+}
--- app/src/main/java/com/benr/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/benr/coderswag/controller/ProductsActivity.kt
@@ -1,17 +1,53 @@
 package com.benr.coderswag.controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.benr.coderswag.utilities.EXTRA_CATEGORY
 import com.benr.coderswag.R
+import com.benr.coderswag.adapters.ProductsAdapter
+import com.benr.coderswag.services.DataService
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var productsAdapter: ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         // Get the category title from the intent extra
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+
+        val products = DataService.getProducts(categoryType!!)
+
+        productsAdapter = ProductsAdapter(this, products)
+
+        val productsListView = findViewById<RecyclerView>(R.id.productsListView)
+        productsListView.adapter = productsAdapter
+
+        var spanCount = 2
+
+        // Get the current orientation of the device (landscape or portrait)
+        val orientation = resources.configuration.orientation
+
+        // If landscape, increase the column size to 3
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        // Get the width of the device
+        val screenWidth = resources.configuration.screenWidthDp
+
+        // If extra large device (i.e. width is greater than 720dp), increase column size to 3 (and landscape column should still be 3)
+        if (screenWidth > 720) {
+            spanCount = 3
+        }
+
+        // Create a horizontal grid (grid layout), not a list (linear layout)
+        val gridLayoutManager = GridLayoutManager(this, spanCount)
+        productsListView.layoutManager = gridLayoutManager
     }
 }
